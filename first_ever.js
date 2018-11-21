671a28f4d4321005ecec66dddc873315a88f8fd7
classroom <classroom@feckc.org>
2018-11-21 11:19:24 -0600

    second day second push

---
+++ first_ever.js
@@ -0,0 +1,20 @@
+function dateAndTime() {
+    document.getElementById('dateAndTime').innerHTML = Date()
+}
+
+function randomNumber(){
+    SomeNumber = Math.random(); 
+    document.getElementById('randomNumber').innerHTML = SomeNumber 
+}
+
+function conosolePrint(){
+    console.log("Hello Miri");
+    }
+
+function alertPrint(){
+    alert("Hello Miri");
+    }
+
+function documentPrint(){
+    document.write("Hello Miri")
+    }
